ebf9d5f7464ac9101d3954b3111a6f1d65ffacec
nastia <mka@A34PC13>
2025-02-25 12:35:08 +0200

    added menu

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hi bitches! Do you want some coffee?");
+Console.WriteLine("1. zavantazhennia avtomatu vodoyu, kofiyok, chayok, cukor");
+Console.WriteLine("2.vyvid statystyky nayavnosti skladovyh dla napoyiv");
+Console.WriteLine("3. zmina cin na napoi");
+Console.WriteLine("4.vyluchennia gotivki");
