0f3da13c758b23fb829b5b2b67f711f1cd6c157e
mka <mka@A34PC13>
2025-03-04 14:56:12 +0200

    shos tam

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
+/*4. ------------------- Автомат гарячих напоїв. 
+Створити ієрархію класів для подання гарячих напоїв Автомату(кава, чай, капучіно та ін.)
+Створити програму для роботи Автомату у  режимах адмін та користувач
+У режимі адміна передбачити 	
+	Завантаження автомату водою, кава, чай, цукор
+	Вивід статистики наявності складових для приготування напоїв
+	Зміна цін на напої
+	Вилучення кешу
+У режимі користувача передбачити*/
+
+//Замовлення напою(+ оплата)	
 Console.WriteLine("Hi bitches! Do you want some coffee?");
 Console.WriteLine("1. zavantazhennia avtomatu vodoyu, kofiyok, chayok, cukor");
 Console.WriteLine("2.vyvid statystyky nayavnosti skladovyh dla napoyiv");
